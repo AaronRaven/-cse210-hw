836e2365f2d0c11fb1b8833684ac07883c0cd0a0
AaronRaven <arunas.rancevas@gmail.com>
2023-12-03 21:22:21 +0200

    homework

---
+++ final/Foundation4/Activity.cs
@@ -0,0 +1,21 @@
+public abstract class Activity
+{
+    protected string _date;
+    protected int _length;
+    public Activity(string date, int length)
+    {
+        _date = date;
+        _length = length;
+    }
+    public abstract float GetDistance();
+    public abstract float GetSpeed();
+    public abstract float GetPace();
+    public string GetSummary()
+    {
+        string type = GetType().Name.Replace("Activity", "");
+        return  $"{_date} {type} ({_length} min): " +
+                $"Distance: {GetDistance():F1} km, " +
+                $"Speed: {GetSpeed():F1} kph, "+
+                $"Pace: {GetPace():F1} min per km";
+    }
+}
--- final/Foundation4/Program.cs
+++ final/Foundation4/Program.cs
@@ -1,9 +1,25 @@
 using System;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation4 World!");
+        Random random = new Random();
+        List<Activity> activities = new List<Activity>();
+
+            RunningActivity runningActivity = new RunningActivity("2023-12-03", 120, 12);
+            activities.Add(runningActivity);
+
+            CyclingActivity cyclingActivity = new CyclingActivity("2023-12-03", 120, 12);
+            activities.Add(cyclingActivity);
+
+            SwimmingActivity swimmingActivity = new SwimmingActivity("2023-12-03", 120, 12);
+            activities.Add(swimmingActivity);
+
+        foreach (Activity activity in activities)
+            {
+                Console.WriteLine(activity.GetSummary());
+            }
     }
 }
